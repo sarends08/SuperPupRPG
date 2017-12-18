7930447c553c076f3771e603b213e12016ed5cc8
sarends08 <saraharends08@gmail.com>
2017-12-18 06:19:32 -0700

    Passed through props from parent

--- src/Choices.js
+++ src/Choices.js
@@ -2,13 +2,28 @@ import React, {Component} from 'react';
 
 class Choices extends Component {
 	render(){
+		const choices = this.props.choices
 		return(
 			<div>
-				<button className = 'button'>Choice 1</button>
-				<button>Choice 2</button>
+				<a href= '#'
+				style={{
+					fontFamily: "'Press Start 2P', cursive",
+					textDecoration: 'none',
+				}}
+				>{choices}</a>
 			</div>
 			)
 	}
 }
 
+
+// const options = {
+// 	stage1: ['Option 1', 'Option 2', 'Option 3'],
+// 	stage2: ['Option 1', 'Option 2', 'Option 3'],
+// 	stage3: ['Option 1', 'Option 2', 'Option 3'],
+// 	stage4: ['Option 1', 'Option 2', 'Option 3'],
+// 	stage5: ['Option 1', 'Option 2', 'Option 3']
+// }
+
 export default Choices
+
--- src/Text.js
+++ src/Text.js
@@ -2,12 +2,25 @@ import React, {Component}  from 'react';
 
 class Text extends Component {
 	render(){
+		const text = this.props.text
 		return (
-			<div className = 'text'>
-			Text goes here.
+			<div className = 'text'
+
+			style= {{
+				padding: '5%',
+				fontFamily: "'Press Start 2P', cursive",
+				fontSize: '1em',
+				textAlign: 'center',
+				color: '#26004d'
+			}}>{text}
 			</div>
 			)
 	}
 }
+
+// const Story = {
+// 	story: "The story is starting, you have 3 options",
+// 	show: false
+// }
 
 export default Text;
