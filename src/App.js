a27d380554397dab956a4f839616255980f60ccb
sarends08 <saraharends08@gmail.com>
2017-12-18 06:12:31 -0700

    Added state to App component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Text from "./Text";
 import './App.css';
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'You suddenly find yourself as a dopey hound pup named Cooper. Looking around you see that you are trapped in a backyard area, and need to find your way to the doghouse to get comfy. Unfortunately, there are several obstacles which will disrupt your path to comfiness.',
+      choices: ['Move forward >']
+    }
+  }
   render() {
     const containerStyle = {
     display: 'flex',
@@ -20,16 +26,20 @@ class App extends Component {
     return (
      <div className= 'container' style={containerStyle}>
        <h1 style= 
-       {{fontFamily: 'Bungee'}}
-       >Super Pup RPG</h1>
+       {{fontFamily: 'Bungee',
+         fontSize: '4em',
+         color: '#00ffcc',
+         textShadow: '3px 2px #00b38f' }}
+       >Super Pup <span style={{
+        color: 'blue'
+       }}>RPG</span></h1>
        <Image />
-       <Text />
-       <Choices />
+       <Text text={this.state.text}/>
+       <Choices choices={this.state.choices}/>
      </div>
     );
   }
 }
-
 
 
 export default App;
