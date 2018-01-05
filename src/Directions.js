526bdcaff64401080c27176fee86d04532b15f73
sarends08 <saraharends08@gmail.com>
2018-01-05 09:39:50 -0700

    Added functionality to hide/show different stages, extracted buttons from component, extracted First & Second Instance to own file, added sprites

--- src/Directions.js
+++ src/Directions.js
@@ -1,104 +1,206 @@
 import React, { Component } from 'react';
-import Treasure from './Treasure';
-import MonsterInstance from './MonsterInstance';
+import {Treasure, BackButton, AcceptItem, TreasureStats} from './Treasure';
+import {MonsterInstance, AttackResult, Winner, Loser, Reload} from './MonsterInstance';
+import {FirstInstanceText, StartGame, GAMETEXT} from './FirstInstanceText';
+import {SecondInstanceText, GAMETEXTB} from './SecondInstanceText';
+import {VacuumMonster, CloudMonster, KittyMonster} from './Image'
+import './Directions.css'
 
 export class Directions extends Component {
   constructor(props){
     super(props);
     this.state = {
-      direction: ''
+      direction: '',
+      showInstance: true,
+      showStartGame: false,
+      counterToChangeInstance: 0
     };
     this.directionState = this.directionState.bind(this);
     this.goBack = this.goBack.bind(this);
+    this.hideInstance= this.hideInstance.bind(this);
+    this.hideStartGame= this.hideStartGame.bind(this);
+    this.changeInstance= this.changeInstance.bind(this);
+    this.update= this.update.bind(this);
   }
 
    directionState(e) {
      this.props.hideButtons();
      this.props.hideStory();
+     this.hideStartGame();
     return this.setState({direction: e.target.value});
   }
   
   goBack(){
     this.props.hideButtons();
     this.props.hideStory();
+    this.hideStartGame();
+    if(this.state.showInstance){
+    	this.hideInstance();
+    }
+    return this.setState({direction: ''});
   }
+
+  hideInstance(){
+  	this.setState({showInstance: !this.state.showInstance})
+  }
+
+  hideStartGame(){
+  	this.setState({showStartGame: !this.state.showStartGame})
+  }
+
+  changeInstance(){
+  	this.setState({counterToChangeInstance: this.state.counterToChangeInstance + 1});
+  	this.setState({direction: ''});
+  }
+
+  update(){
+  	this.props.toggleFoundItem();
+  	this.props.updateStats();
+  };
   
   render(){
     const optionArray = [];
     
     const {showButtons, hideButtons, showStory, foundItem, toggleFoundItem,
-    hideStory, gameText, updateStats, direction, directionState}     
+    hideStory, gameText, updateStats, directionState}     
     = this.props;
+
+    const {showInstance, showStartGame, direction} = this.state;
+
+    let vacuumGif = null;
+    let vacuum = <VacuumMonster/>
     
-    this.props.gameText.forEach((gameText) => {
-      if(this.state.direction === gameText.direction)
-        optionArray.push(
-          <StartGame key = {gameText.text} text = {gameText.text}/>
-          );
-      });
-    
-    !showStory && <MonsterInstance/>
-    
+    let cloudGif = null;
+    let cloud = <CloudMonster/>
+
+    let kittyGif = null;
+    let kitty = <KittyMonster/>
+
+    let treasureComponent = null;
+    let treasure = <Treasure
+		    		dogHP={this.props.dogHP}
+			      	itemHP={this.props.itemHP}
+			      	itemAtk={this.props.itemAtk}
+			      	direction={this.state.direction} 
+			      	updateStats={updateStats}
+			      	toggleFoundItem={toggleFoundItem}
+			      	hideButtons={this.props.hideButtons}
+			      	hideStory={this.props.hideStory}
+			      	goBack={this.goBack}
+			      	update={this.update}
+			      	foundItem={this.props.foundItem}
+			      	counterToChangeInstance={this.state.counterToChangeInstance}/>;
+
+	let monsterComponent = null; 
+	let monster = <MonsterInstance
+	          dogHP={this.props.dogHP}
+	          monstHP={this.props.monstHP}
+	          turnToggle={this.props.turnToggle}
+	          attack={this.props.attack} 
+	          storyProgression={this.props.storyProgression}
+	          hideInstance={this.hideInstance}
+	          hideStartGame={this.hideStartGame}
+	          showInstance={this.state.showInstance}
+	          changeInstance={this.changeInstance}
+	          direction={this.state.direction}
+	          attackType={this.props.attackType}
+	          clearAttackType={this.props.clearAttackType}
+	          counterToChangeInstance={this.state.counterToChangeInstance}
+	          hideStory={this.props.hideStory}
+	          hideButtons={this.props.hideButtons}
+	          resetMonster={this.props.resetMonster}
+	          addToStoryCounter={this.props.addToStoryCounter}
+	          goBack={this.goBack}/>
+ 
+
+    //Try to refactor
+    if (this.state.direction === 'Left' && this.state.counterToChangeInstance === 0){
+		treasureComponent = treasure;
+
+    }else if (this.state.direction === 'Center' && this.state.counterToChangeInstance === 0){
+    	treasureComponent = treasure;
+    }else if (this.state.direction === 'Center' && this.state.counterToChangeInstance === 1){
+    	treasureComponent = treasure;
+    }else if (this.state.direction === 'Center' && this.state.counterToChangeInstance === 2){
+    	treasureComponent = treasure;	
+  	}else if (this.state.direction === 'Right' && this.state.counterToChangeInstance === 0) {
+  		monsterComponent = monster;
+      vacuumGif = vacuum;
+  	}else if (this.state.direction === 'Left' && this.state.counterToChangeInstance === 1){
+  		monsterComponent = monster;
+      cloudGif = cloud;
+  	}else if (this.state.direction === 'Left' && this.state.counterToChangeInstance === 2){
+    	cloudGif = cloud;
+      treasureComponent = treasure;	
+  	}else if (this.state.direction === 'Right' && this.state.counterToChangeInstance === 1){
+  		monsterComponent = monster;
+      kittyGif = kitty;
+  	}else if (this.state.direction === 'Right' && this.state.counterToChangeInstance === 2){
+  		monsterComponent = monster;
+      kittyGif = kitty;
+  	}
+
+
+
     return (
     <div>
-      { showButtons ? 
-       <div>   
-        <button onClick={this.directionState} value='Left'>Left</button>
-        <button onClick={this.directionState} value='Center'>Center</button>
-        <button onClick={this.directionState} value='Right'>Right</button>
-        </div>
-      : 
-      <div>
-        <div>{optionArray}</div>
-        <Treasure 
-          dogHP={this.props.dogHP}
-          itemHP={this.props.itemHP}
-          itemAtk={this.props.itemAtk}
-          direction={this.state.direction} 
-          updateStats={updateStats}
-          toggleFoundItem={toggleFoundItem}
-          hideButtons={this.props.hideButtons}
-          hideStory={this.props.hideStory}
-          />
-          { this.state.direction === 'Right' ?
-        <MonsterInstance
-          dogHP={this.props.dogHP}
-          monstHP={this.props.monstHP}
-          turnToggle={this.props.turnToggle}
-          attack={this.props.attack} 
-          goBack={this.goBack}/>
-          : null }
-       </div>
-       }
+      	{ showButtons ? 
+       	<DirectionButtons 
+       	directionState={this.directionState}/>
+      : null }
+	    <div>
+	        <div>
+	        	{this.state.counterToChangeInstance === 0 ?
+	        	<FirstInstanceText 
+	        	direction={this.state.direction}
+	        	showStartGame={this.state.showStartGame}
+	        	gameText={this.props.gameText}/>
+	        	: null}
+	        	{this.state.counterToChangeInstance === 1 ?
+	        	<SecondInstanceText
+	        	direction={this.state.direction}
+	        	showStartGame={this.state.showStartGame}
+	        	gameTextB={this.props.gameTextB}/>
+	        	: null}
+	        	{this.state.counterToChangeInstance === 2 ?
+	        	<SecondInstanceText
+	        	direction={this.state.direction}
+	        	showStartGame={this.state.showStartGame}
+	        	gameTextB={this.props.gameTextB}/>
+	        	: null}
+	        </div>
+	        <div className='div-container'>
+	        {treasureComponent}
+	 		    </div>
+          <div className='div-container'>
+          {vacuumGif}
+          {cloudGif}
+          {kittyGif}
+           </div>
+	        {monsterComponent}
+	    </div>
   </div>
     );
   }
 }
 
-export class StartGame extends Component {
-  render(){   
-    return (
-      <div>
-        <ul style={{listStyle: 'none'}}>
-          <li>{this.props.text}</li>
-        </ul>
-      </div>
-    );
-  }
+
+
+export class DirectionButtons extends Component{
+	render(){
+		const {directionState} = this.props;
+		return(
+			<div className='div-container'>   
+	        <button className='dir-btn' onClick={directionState} value='Left'>Left</button>
+	        <button className='dir-btn' onClick={directionState} value='Center'>Center</button>
+	        <button className='dir-btn' onClick={directionState} value='Right'>Right</button>
+        </div>
+			)
+	}
 }
 
-export const GAMETEXT = [
-  {
-    direction: 'Left', 
-    text: 'You found treasure'
-  },
-  {
-    direction: 'Center',
-    text: "There's nothing here"
-  },
-  {
-    direction: 'Right',
-    text: 'Monster encounter'
-  }
-];
+
+
+
+
 
