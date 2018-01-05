923b68de7164d740ba7043b00fb9e324b780a635
sarends08 <saraharends08@gmail.com>
2018-01-05 09:41:11 -0700

    Added all the sprites for the game and removed placeholder image

--- src/Image.js
+++ src/Image.js
@@ -1,14 +1,64 @@
-import React, {Component} from 'react';
-import dog from './dog.png';
+import React from 'react';
+import Cooper from './Cooper.gif';
+import Bone from './Bone.gif';
+import Vacuum from './Vacuum.gif';
+import Ball from './Ball.gif';
+import Cloud from './Cloud.gif';
+import Kitty from './Kitty.gif';
+import House from './Doghouse.gif';
 
-class Image extends Component {
-	render(){
-		return(
-			<div className='image'>
-			<img src = {dog} alt="Cartoon dog"/>
-			</div>
-			)
-	}
+export const Dog = props => {
+	return(
+		<div className='image'>
+		<img src = {Cooper} alt="sprite of a dog"/>
+		</div>
+		)
 }
 
-export default Image;
+export const DogBone = props => {
+	return(
+		<div className='image'>
+		<img src = {Bone} alt="sprite of a dog-bone"/>
+		</div>
+		)
+}
+
+export const VacuumMonster = props => {
+	return(
+		<div className='image'>
+		<img src = {Vacuum} alt="sprite of a vacuum"/>
+		</div>
+		)
+}
+
+export const DogBall = props => {
+	return(
+		<div className='image'>
+		<img src = {Ball} alt="sprite of a ball"/>
+		</div>
+		)
+}
+
+export const CloudMonster = props => {
+	return(
+		<div className='image'>
+		<img src = {Cloud} alt="sprite of a cloud with lightning"/>
+		</div>
+		)
+}
+
+export const KittyMonster = props => {
+	return(
+		<div className='image'>
+		<img src = {Kitty} alt="sprite of a black cat"/>
+		</div>
+		)
+}
+
+export const DogHouse = props => {
+	return(
+		<div className='image'>
+		<img src = {House} alt="sprite of a red dog house"/>
+		</div>
+		)
+}
