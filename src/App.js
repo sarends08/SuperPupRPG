1e97ea8f518bc249be2e68c9edd2c9beb3eb7c37
sarends08 <saraharends08@gmail.com>
2018-01-05 09:33:15 -0700

    Added functionality to move beyond first instance, added in sprites and updated the story

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {Directions, StartGame, GAMETEXT} from "./Directions";
-import Image from "./Image";
+import {Directions, DirectionButtons} from "./Directions";
+import {Dog, DogHouse} from "./Image";
 import './App.css';
 
 export class App extends Component {
     constructor(props){
     super(props);
     this.state = {
-      itemHP: 10,
+      itemHP: 20,
       dogHP: 100,
       monstHP: 70,
       itemAtk: 5,
@@ -16,8 +16,9 @@ export class App extends Component {
       counter: 0,
       showStory: true,
       showButtons: true,
-      foundItem: true,
-      playerTurn: true
+      foundItem: false,
+      playerTurn: true,
+      attackType: ''
     };
     this.handleClick = this.handleClick.bind(this);
     this.hideStory = this.hideStory.bind(this);
@@ -27,31 +28,60 @@ export class App extends Component {
     this.turnToggle = this.turnToggle.bind(this);
     this.attack = this.attack.bind(this);
     this.updateFight = this.updateFight.bind(this);
+    this.storyProgression = this.storyProgression.bind(this);
+    this.clearAttackType = this.clearAttackType.bind(this);
+    this.resetMonster = this.resetMonster.bind(this);
+    this.addToStoryCounter = this.addToStoryCounter.bind(this);
   }
-  
+
   handleClick() {
     if(this.state.counter < 2)
       return this.setState({counter: this.state.counter + 1});
   }
-  
+
   hideStory(){
-    this.state.showStory ? this.setState({showStory:false}) : this.setState({showStory: true});
+    this.setState({showStory: !this.state.showStory});
+  }
+
+  addToStoryCounter(){
+    this.setState({counter: this.state.counter + 1})
+  }
+
+  storyProgression(){
+    this.hideStory()
+    this.hideButtons()
+    if(this.state.monstHP <= 0 && !this.state.showStory){
+      this.setState({counter: this.state.counter + 1, monstHP: 100, itemHP: 120, itemAtk: 10});
+    if (this.state.foundItem){
+      this.toggleFoundItem();
+    }
+  }
+}
+
+  resetMonster(){
+    this.hideStory();
+    this.hideButtons();
+    if (this.state.monstHP <= 0 && !this.state.showStory){
+      this.setState({monstHP: 80, monstAtk: 20});
+    }
   }
   
   hideButtons(){
-    this.state.showButtons ? this.setState({showButtons:false}) : this.setState({showButtons: true});
+    this.setState({showButtons: !this.state.showButtons});
   }
   // if button is pressed 3 times stats are added again
   toggleFoundItem(){
-    this.state.foundItem ? this.setState({foundItem: false}) :
-    this.setState({foundItem: true});
+    this.setState({foundItem: !this.state.foundItem});
   }
   
   updateStats(){
-    if (this.state.foundItem) 
-      this.setState(prevState => ({dogHP: prevState.dogHP + this.state.itemHP}));
-      this.setState(prevState => ({dogAtk: prevState.dogAtk + this.state.itemAtk}));
-    };
+    if (!this.state.foundItem) {
+      return(
+        this.setState(prevState => ({dogHP: prevState.dogHP + this.state.itemHP})),
+        this.setState(prevState => ({dogAtk: prevState.dogAtk + this.state.itemAtk}))
+      );
+    }
+  };
   
   turnToggle(){
     this.state.playerTurn ? this.setState({playerTurn: false}) : this.setState({playerTurn: true})
@@ -68,63 +98,71 @@ export class App extends Component {
   attack(activeHP, activeAtk){
     const random = Math.floor((Math.random() * 10) + 1);
     if(random >= 1 && random <= 2){
-        return console.log('MISS')
+        return this.setState({attackType: 'MISS!'});
     }else if (random >= 3 && random <=4){
       return (
         this.updateFight(-5),
-        console.log('ATK - 5'));
+        this.setState({attackType: 'PARRY!'}));
     }else if (random >=5 && random <= 8){
       return (
         this.updateFight(0),
-        console.log('ATK'));
+        this.setState({attackType: 'HIT!'}));
     }else if (random >=8 && random <= 10){
       return (
-        console.log('ATK + 5'),
-        this.updateFight(5));
+        this.updateFight(5),
+        this.setState({attackType: 'CRITICAL HIT!'}));
     }
   }
+
+  clearAttackType(){
+    if(this.state.monstHP <= 0){
+      this.setState({attackType: ''})
+    }
+  }
+
 
   render() {
     const arrayStory = []
     const {gameText, story} = this.props;
-    const {showButtons, showStory, dogAtk, dogHP, monstHP, itemHP, itemAtk} = this.state;
+    const {showButtons, showStory, dogAtk, dogHP, monstHP,
+    itemHP, itemAtk, attackType, foundItem} = this.state;
 
-    const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
-    paddingTop: '10%',
-  };
+    let dogGif = <Dog/>
+    let noDog = null;
 
+    let dogHouseGif = null;
+    let doghouse = <DogHouse/>
+
+
+    if (this.state.counter === 4) {
+      dogHouseGif = doghouse;
+      dogGif = noDog;
+    }
+
+    
     story.forEach((story) => {
      if (this.state.counter === story.id)
        arrayStory.push(
      <Text
        hideStory = {this.hideStory}
        text = {story.text}
-       key = {story.text} />
+       key = {story.id} />
        );
    });
 
     return (
-     <div className= 'container' style={containerStyle}>
-       <h1 style= 
-       {{fontFamily: 'Bungee',
-         fontSize: '4em',
-         color: '#00ffcc',
-         textShadow: '3px 2px #00b38f' }}
-       >Super Pup <span style={{
-        color: 'blue'
-       }}>RPG</span></h1>
-       <Image />
-       <div>
+     <div className= 'container'>
+       <h1 className= 'title'>Super Pup <span className= 'title-span'>RPG</span></h1>
+       
+       <div className= 'inner-container'>
         { showStory ? 
-        <h1 onClick={this.handleClick}>{arrayStory}</h1>
+        <div className= 'story-text'
+        onClick={this.handleClick}>
+        {dogGif}
+        {dogHouseGif}
+        {arrayStory}</div>
         : null }
-          { this.state.counter === 2 ?
+          { this.state.counter >= 2 ?
           <div>
             <Directions
               dogAtk={dogAtk}
@@ -140,6 +178,14 @@ export class App extends Component {
               hideButtons={this.hideButtons}
               showButtons={showButtons}
               toggleFoundItem={this.toggleFoundItem}
+              foundItem={this.state.foundItem}
+              attackType={attackType}
+              playerTurn={this.state.playerTurn}
+              storyProgression={this.storyProgression}
+              gameTextB={this.props.gameTextB}
+              clearAttackType ={this.clearAttackType}
+              resetMonster={this.resetMonster}
+              addToStoryCounter={this.addToStoryCounter}
               updateStats={this.updateStats}/>
           </div>
             : null }
@@ -151,7 +197,7 @@ export class App extends Component {
 
 export class Text extends Component {
   render(){
-    return <div>{this.props.text}</div>;   
+    return <p>{this.props.text}</p>;   
   }
 }
 
@@ -164,13 +210,20 @@ export const STORY = [
             },
             {
               id: 1,
-              text: 'You suddenly find yourself as a dopey hound pup named Cooper. Looking around you see that you are trapped in a backyard area, and need to find your way to the doghouse to get comfy. Unfortunately, there are several obstacles which will disrupt your path to comfiness.'
+              text: 'You suddenly find yourself as a dopey hound dog named Cooper. Looking around, you see that you are trapped inside the house, but the best place to nap is outside in the doghouse! What will you do?'
             },
             {
               id: 2,
-              text: 'You look around the room and decide which way to go'
+              text: 'You look around the room and decide which way to go.'
+            },
+            {
+              id: 3, 
+              text: 'Whew! That was close, but you made it through the doggie-door and into the backyard! Where to now?'
+            },
+            {
+              id: 4, 
+              text: "You found the doghouse! Reward yourself with a nice long nap."
             }
-
 ]
 
 
